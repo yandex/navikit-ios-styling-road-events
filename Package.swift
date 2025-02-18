3aa8a01ab060518f0e56950e4ee24c4cc694a984
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2025-02-18 15:19:31 +0300

    Publish swift package YMKStylingRoadEvents version 4.11.0

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "YMKStylingRoadEvents",
+    defaultLocalization: "en",
+    platforms: [.iOS("13.0")],
+    products: [
+        .library(
+            name: "YMKStylingRoadEvents",
+            type: .static,
+            targets: [
+                "YMKStylingRoadEvents",
+                "YMKStylingRoadEventsResources"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/yandex/navikit-ios", exact: "4.11.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "YMKStylingRoadEvents",
+            url: "https://maps-ios-pods-public.s3.yandex.net/YMKStylingRoadEvents-4.11.0.framework.zip",
+            checksum: "96e98d8893702dced2ce8f840d6634822d823b75f7ecba5424d3818b2eadc0e6"
+        ),
+        .target(
+            name: "YMKStylingRoadEventsResources",
+            dependencies: [
+                .product(name: "YandexMapsMobileNavikit", package: "navikit-ios"),
+            ],
+            path: "Resources",
+            resources: [.process("Contents")]
+        )
+    ]
+)
---
+++ Resources/Resources.m
@@ -0,0 +1,13 @@
+#import <resource_bundle_accessor.h>
+
+@interface YMKStylingRoadEventsSwiftResources : NSObject
+@end
+
+@implementation YMKStylingRoadEventsSwiftResources
+
++ (NSBundle *)mapkitResourcesBundle
+{
+    return SWIFTPM_MODULE_BUNDLE;
+}
+
+@end
