db597e8719714c033daf202d1f36e1ab8341b87a
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2025-10-10 22:13:59 +0300

    Publish swift package YMKStylingRoadEvents version 4.24.0

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/yandex/navikit-ios", exact: "4.23.0"),
+        .package(url: "https://github.com/yandex/navikit-ios", exact: "4.24.0"),
     ],
     targets: [
         .binaryTarget(
             name: "YMKStylingRoadEvents",
-            url: "https://maps-ios-pods-public.s3.yandex.net/YMKStylingRoadEvents-4.23.0.framework.zip",
-            checksum: "5a468ef0afab62de65678c27231dbd9aa9ae96f7687d8ad62bca1aecdd7ca84f"
+            url: "https://maps-ios-pods-public.s3.yandex.net/YMKStylingRoadEvents-4.24.0.framework.zip",
+            checksum: "f790e0b9e05df86fd6472e616c6d0cecf768d9c98a83b9ab83373e1d51dd1ba5"
         ),
         .target(
             name: "YMKStylingRoadEventsResources",
